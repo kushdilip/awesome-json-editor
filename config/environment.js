b9fca246a7575095fa4a38b1648e7d246160c019
Dilip <dilip@artoo.in>
2016-01-10 12:35:13 +0530

    included ember-electron addon.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'awesome-json-editor',
     environment: environment,
+    baseURL: '/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
